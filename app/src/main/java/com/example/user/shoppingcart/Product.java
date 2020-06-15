e85eb3fae8a5db9e60d2d3c0a762999d4a15c669
anju-abraham <46350051+anju-abraham@users.noreply.github.com>
2020-06-15 23:45:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/shoppingcart/Product.java
@@ -0,0 +1,27 @@
+package com.example.user.shoppingcart;
+
+/**
+ * Created by user on 28-Feb-19.
+ */
+
+
+
+import android.graphics.drawable.Drawable;
+
+public class Product {
+
+    public String title;
+    public Drawable productImage;
+    public String description;
+    public double price;
+    public boolean selected;
+
+    public Product(String title, Drawable productImage, String description,
+                   double price) {
+        this.title = title;
+        this.productImage = productImage;
+        this.description = description;
+        this.price = price;
+    }
+
+}
